52a9f67b545a4b9cffc0a7da1e934c3a9c1f509f
sergio-ayala123 <venturian6@gmail.com>
2022-02-13 21:29:38 -0700

    e
    e

--- EaterAPI2022/Controllers/MoveBotController.cs
+++ EaterAPI2022/Controllers/MoveBotController.cs
@@ -50,14 +50,7 @@ namespace EaterAPI2022.Controllers
                     Board left = list.FirstOrDefault(x => x.location.column == current.location.column - 1 && x.location.row == current.location.row);
                     Board right = list.FirstOrDefault(x => x.location.column == current.location.column + 1 && x.location.row == current.location.row);
 
-                    if (up.isPillAvailable == false && down.isPillAvailable == false && left.isPillAvailable == false)
-                    {
-                        botmove = await httpClient.GetFromJsonAsync<MoveResult>($"https://hungrygame.azurewebsites.net/move/right/?token={state.Token}");
-                        list = null;
-
-                        //return current;
-                    }
-                    else if (up != null && up.isPillAvailable == true)
+                    if (up != null && up.isPillAvailable == true)
                     {
                         botmove = await httpClient.GetFromJsonAsync<MoveResult>($"https://hungrygame.azurewebsites.net/move/up/?token={state.Token}");
                         list = null;
@@ -79,6 +72,13 @@ namespace EaterAPI2022.Controllers
                         //return current;
                     }
 
+                    else if (up.isPillAvailable == false && down.isPillAvailable == false && left.isPillAvailable == false)
+                    {
+                        botmove = await httpClient.GetFromJsonAsync<MoveResult>($"https://hungrygame.azurewebsites.net/move/right/?token={state.Token}");
+                        list = null;
+
+                        //return current;
+                    }
 
 
                     list = null;
